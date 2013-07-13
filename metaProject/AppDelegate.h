da1d7f2901f52f53b7b2a332d44a69ef9da34e38
Dmytro Krasylnikov <krasylnikov@gmail.com>
2013-07-13 17:11:55 +0300

    Initial Commit

---
+++ metaProject/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  metaProject
+//
+//  Created by dimakey on 13.07.13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
