ffa29d412e31969ccaa3ee9f4d2d1c3f3ca09246
Dmytro Krasylnikov <krasylnikov@gmail.com>
2013-07-17 17:00:49 +0300

    copyright

--- DebugTableViewController.m
+++ DebugTableViewController.m
@@ -3,8 +3,8 @@
 //  DebugTableViewController.m
 //  metaProject
 //
-//  Created by dimakey on 13.07.13.
-//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//  Created by krasylnikov on 13.07.13.
+//  Copyright (c) 2013 Design and Test lab. All rights reserved.
 //
 //==============================================================================
 
