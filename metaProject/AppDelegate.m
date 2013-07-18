16cb0c94470d9a5fe079ac67bf59545bf31d9823
Dmytro Krasylnikov <krasylnikov@gmail.com>
2013-07-18 20:15:58 +0300

    minor fix

--- metaProject/AppDelegate.m
+++ metaProject/AppDelegate.m
@@ -26,7 +26,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[UIScreen.mainScreen bounds]];
+    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
     // Override point for customization after application launch.
     self.debugTableViewController = [DebugTableViewController new];
     
